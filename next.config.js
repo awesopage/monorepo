7f5f47e04f95bca14d6b97ec26a412dc8bfdfbba
Bao D <baocancode@gmail.com>
2023-02-11 03:10:50 +0000

    Add build scripts

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
+import bundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+})
+
 const configFiles = ['next.config.js', '.eslintrc.cjs']
 
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
+const nextConfig = withBundleAnalyzer({
   swcMinify: true,
   reactStrictMode: true,
   poweredByHeader: false,
   eslint: {
     dirs: ['pages', 'scripts', ...configFiles],
   },
-}
+})
 
 export default nextConfig
