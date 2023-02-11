120dee96f6f603c54a5c843e46b38d77f06f1a43
Bao D <baocancode@gmail.com>
2023-02-11 13:15:46 +0000

    Fix missing test.describe

--- tests/api/server/HealthApiHandler.spec.ts
+++ tests/api/server/HealthApiHandler.spec.ts
@@ -2,12 +2,14 @@ import { expect, test } from '@playwright/test'
 
 import type { HealthStatusDTO } from 'pkg-app-shared/src/server/HealthStatusDTO'
 
-test('Can get health status by API', async ({ page }) => {
-  const getHealth = await page.request.get('/api/server/health')
-  const healthStatus: HealthStatusDTO = await getHealth.json()
+test.describe('HealthApiHandler', () => {
+  test('should return ok health status', async ({ page }) => {
+    const getHealth = await page.request.get('/api/server/health')
+    const healthStatus: HealthStatusDTO = await getHealth.json()
 
-  expect(healthStatus).toEqual({
-    ok: true,
-    database: true,
+    expect(healthStatus).toEqual({
+      ok: true,
+      database: true,
+    })
   })
 })
