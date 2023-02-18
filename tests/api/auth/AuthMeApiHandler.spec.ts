947e973d7ab7adc7b8ae4f76b6df0b51562e4796
Bao D <baocancode@gmail.com>
2023-02-18 10:47:19 +0000

    Update test titles

--- tests/api/auth/AuthMeApiHandler.spec.ts
+++ tests/api/auth/AuthMeApiHandler.spec.ts
@@ -2,7 +2,7 @@ import type { AuthMeDTO } from 'pkg-app-shared/src/auth/AuthMeDTO'
 import { expect, test, useTestUser } from 'tests/common/TestUtils'
 
 test.describe('AuthMeApiHandler', () => {
-  test.describe('when signed in', () => {
+  test.describe('given signed in', () => {
     useTestUser('user1')
 
     test('should return current user', async ({ page }) => {
@@ -16,8 +16,8 @@ test.describe('AuthMeApiHandler', () => {
     })
   })
 
-  test.describe('when not signed in', () => {
-    test('should return no user when not signed in', async ({ page }) => {
+  test.describe('given not signed in', () => {
+    test('should return no user', async ({ page }) => {
       const getAuthMe = await page.request.get('/api/auth/me')
       const authMe: AuthMeDTO = await getAuthMe.json()
 
